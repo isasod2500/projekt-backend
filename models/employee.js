2679d113fd35145423f664cf7c8db2df590015e4
isso2500 <isso2500@student.miun.se>
2026-05-19 17:11:42 +0200

    Automatic creation of username and email implemented. Using nanoid for a custom ID, looks nicer and shorter than the automatic mongoDB id.

--- models/employee.js
+++ models/employee.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const { nanoid } = require("nanoid");
 
 //Modell för anställda
 const employeeSchema = new mongoose.Schema({
     _id: {
         type: String,
-        default: () => nanoid(),
+        default: () => nanoid(5),
     },
     username: {
         type: String,
--- routes/routes.js
+++ routes/routes.js
@@ -15,15 +15,13 @@ mongoose.connect(process.env.DATABASE).then(() => {
 //Importera mall för användare och maträtt
 const Employee = require("../models/employee.js");
 const Dish = require("../models/dish.js");
+const { customAlphabet } = require("nanoid");
+const nanoid = customAlphabet(`1234567890`, 4)
 
 function validateRegister(req, res, next) {
-    const { username, password, firstname, surname, email, admin } = req.body;
+    const { password, firstname, surname, admin } = req.body;
 
     const errors = []
-
-    if(!username || username.length < 6) {
-        errors.push(`Användarnamn måste vara minst 6 tecken`)
-    }
 
     if (!password || password.length < 6) {
         errors.push(`Lösenord måste vara minst 8 tecken`)
@@ -35,10 +33,6 @@ function validateRegister(req, res, next) {
 
     if(!surname || surname.length < 2) {
         errors.push(`Efternamn måste vara minst 2 tecken`)
-    }
-
-    if(!email || !email.includes("@")) {
-        errors.push(`Felaktigt format på e-post adress`)
     }
 
     if(!admin) {
@@ -53,16 +47,30 @@ function validateRegister(req, res, next) {
 }
 
     //Route för att registrera användare
-    router.post("/register", validateRegister, (req, res) => {
+    router.post("/register", validateRegister, async (req, res) => {
         try {
-            const { username, password, firstname, surname, email, admin } = req.body;
+            const { password, firstname, surname, admin } = req.body;
 
 
-            if (!username || !password || !firstname || !surname || !email || !admin) {
+            if (!password || !firstname || !surname || !admin) {
                 return res.status(400).json({ error: `Invalid input, all fields must be entered.` })
             }
 
+            let first = firstname.slice(0,3).toLowerCase().replace(/[åä]/g, "a").replace(/[ö]/g, "o")
+            let sur = surname.slice(0,3).toLowerCase().replace(/[åä]/g, "a").replace(/[ö]/g, "o")
+
+            const username = `${first}${sur}${nanoid()}`
+            const email = `${username}@E4Haket.se`
+
+            const employee = new Employee({ username, password, firstname, surname, email, admin })
+            await employee.save();
+
+            res.status(201).json({ message: `User successfully created` })
+
         } catch (err) {
+            if (err.code === 11000) {
+                return res.status(409).json({ message: `Username already taken` })
+            }
             res.status(500).json({ error: `${err}` })
         }
     })
